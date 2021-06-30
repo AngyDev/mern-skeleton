5bfba2fb859528fab30a77d63ee44a4edb6f35a5
Angela Busato <busato.angela@gmail.com>
2021-06-30 13:15:25 +0200

    [add] authorization routes and errors

--- server/express.js
+++ server/express.js
@@ -6,7 +6,8 @@ import compress from 'compression';
 import cors from 'cors';
 import helmet from 'helmet';
 import Template from '../template';
-import userRouters from './routes/user.routes';
+import userRoutes from './routes/user.routes';
+import authRoutes from './routes/auth.routes';
 
 const app = express();
 
@@ -19,10 +20,21 @@ app.use(helmet());
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.status(200).send(Template());
-});
+// app.get('/', (req, res) => {
+//     res.status(200).send(Template());
+// });
 
 app.use('/', userRoutes);
+app.use('/', authRoutes);
+
+// Catch unauthorised errors
+app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') { // have this error when the token cannot be validated for some reason
+        res.status(401).json({ "error": err.name + ": " + err.message })
+    } else if (err) {
+        res.status(400).json({ "error": err.name + ": " + err.message })
+        console.log(err)
+    }
+})
 
 export default app;
